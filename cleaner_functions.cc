e3fe824afae068d23f59dd7f1594877ffece69b6
Jason Uwaeze <uwaeze@Jasons-MBP-3.attlocal.net>
2020-05-02 16:24:39 -0500

    Adding cleaner functions and header file

---
+++ cleaner_functions.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <map>
+
+
+using namespace std;
+
+void getClassList(){
+  
+  Map<int, string> class_map = new Map<int, string>
+}
---
+++ header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+int main(int argc, char *argv[]);
+
+
+#endif
