b068f7e16993b16c7d3b8d898ab9fd1247d66d04
Jason Uwaeze <uwaeze@Jasons-MBP-3.attlocal.net>
2020-05-02 19:01:58 -0500

    Adding Makefile and updating cleaner functions

--- cleaner_functions.cc
+++ cleaner_functions.cc
@@ -8,5 +8,33 @@ using namespace std;
 
 void getClassList(){
   
-  Map<int, string> class_map = new Map<int, string>
+  map<int, string> class_map;
 }
+void welcomeMessage(){
+  string longest_str = "*\n*\n* A tool for keeping your desktops/folders clutter free!             *";
+  int length = longest_str.length();
+  cout << length << endl;
+  // print repeating border with std::string(int, string) function
+  cout << string(length , '*') << endl;
+  cout << "* Welcome to DTCleaners\n";
+  cout << string(length, ' ') <<  "*";
+  cout << "\n*\n* A tool for keeping your desktops/folders clutter free!                 *" << endl;
+  cout << "* During the semester, things can get really hectic                      *\n";
+  cout << "* Folders start to get disorganized and random items start to \n";
+  cout << "* find a home in your desktop\n* And if your anything like me, you give that, 'i'll clean it later excuse'\n";
+  cout << "*\n*\n* I am creating this tool to make, 'I'll just clean it now',\n* a more frequent response!";
+  cout << "* Lets get start " << endl;
+  cout << "" << endl;
+}
+string getLocation(){
+  string res;
+  cout << "Would you like to clean your desktop or folder?(Y/N):" << endl;
+  cin >> res;
+  cout << "res: " << endl;
+  return "";
+}
+string getFolderName(){
+  return "";
+  
+}
+
--- main.cc
+++ main.cc
@@ -3,6 +3,12 @@
 #include <map>
 #include "header.h"
 
+void welcomeMessage();
+
 int main(int argc, char* argv[]){
+  // welcome user
+  welcomeMessage();
+  // Desktop or Folder
+  
   return 0;
 }
