b139303e627bd165a1d25216bfc0b3f148d077ef
Jason Uwaeze <uwaeze@Jasons-MBP-3.attlocal.net>
2020-05-11 10:25:43 -0500

    Error with comparing strings in main.cc and popen function in cleaner

--- main.cc
+++ main.cc
@@ -2,13 +2,21 @@
 #include <string>
 #include <map>
 #include "header.h"
+#include "stdio.h"
 
 using namespace std;
 
 void welcomeMessage();
 string getLocation();
+void cleanDesktop();
 
 int main(int argc, char* argv[]){
+  if(argv[0] == "./desktop"){
+    cout << "organizing desktop..." << endl;
+    cleanDesktop();
+  } else if(argv[0] == "./folder") {
+    cout << "organizing folder..." << endl;
+  }
   // welcome user
   welcomeMessage();
   // Desktop or Folder
